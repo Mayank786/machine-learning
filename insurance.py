ec6fe463beab1a8a0a0e0b932b1b2ae2a867fdd5
Mayank Pandey <40591573+Mayank786@users.noreply.github.com>
2026-06-16 21:19:59 +0000

    insurace-project

--- insurance.py
+++ insurance.py
@@ -15,3 +15,27 @@ for col in numeric_columns:
     plt.xlabel(col)
     plt.ylabel('Frequency')
     plt.show()
+    #EDA (Exploratory Data Analysis)
+    #Data Cleaning and preprocessing
+    #to not hamper the real data we will create a copy of the original data frame
+df_cleaned = df.copy()
+df_cleaned.drop_duplicates(inplace=True)
+print(df_cleaned.isnull().sum())
+print(df_cleaned.shape)
+print(df_cleaned.dtypes)
+
+df_cleaned['sex'].value_counts() #to check the unique values like Male and male as it counts to 2 diff. values
+print(df_cleaned['smoker'].value_counts())
+print(df_cleaned['sex'].unique())
+
+#Now for for sex column we will convert the values into 0 and 1 where 0 will represent male and 1 will represent female
+df_cleaned['sex'] = df_cleaned['sex'].map({'male': 0, 'female': 1})
+df_cleaned['smoker'] = df_cleaned['smoker'].map({'yes': 1, 'no': 0})
+print(df_cleaned.head())
+#Now we will convert the categorical columns into numerical columns using one-hot encoding
+df_cleaned = pd.get_dummies(df_cleaned, columns=['region'], drop_first=True) #drop_first=True to avoid multicollinearity
+print(df_cleaned.head())
+df_cleaned=df_cleaned.astype(int)
+print(df_cleaned.head())
+#Feature engineering and Extraction
+#BMI Range 18.5 - 24.9 Healthy Weight; 25.0 - 29.9 Overweight; 30.0 or higher Obese
